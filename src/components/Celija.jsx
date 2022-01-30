4eba4122940562e5c656614a9bb1c0642ee0c0f1
minakusic9 <minakusic9@gmail.com>
2022-01-30 14:07:35 +0100

    Kreirana komponenta matrica

---
+++ src/components/Celija.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Celija({ vrednost, onChange, readOnly }) {
+  return (
+    <div className='celija'>
+      {
+        readOnly ? (
+          <span>{vrednost}</span>
+        ) : (
+          <input type="text" className='form-control basic' value={vrednost} onChange={e => {
+
+            const value = Number(e.target.value);
+            if (isNaN(value)) {
+              return;
+            }
+            onChange(value);
+          }} />
+        )
+      }
+    </div>
+  );
+}
--- src/components/Sabiranje.jsx
+++ src/components/Sabiranje.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import Matrica from './Matrica';
 
 export default function Sabiranje() {
-  return <div></div>;
+
+  return (
+    <div className='container mt-2'>
+      <h1 className='text-center'>Sabiranje matrica</h1>
+      <div className='row mt-5'>
+        <div className='col-3'>
+          <Matrica podaci={[[1, 2], [1, 2], [4, 5]]} />
+        </div>
+        <div className='col-1 d-flex align-items-center justify-content-center display-4'>+</div>
+        <div className='col-3'>
+          <Matrica podaci={[[1, 2], [1, 2], [4, 5]]} />
+        </div>
+        <div className='col-1 d-flex align-items-center justify-content-center display-4'>=</div>
+        <div className='col-3'>
+          <Matrica readOnly podaci={[[1, 2], [1, 2], [4, 5]]} />
+        </div>
+      </div>
+    </div>
+  );
 }
